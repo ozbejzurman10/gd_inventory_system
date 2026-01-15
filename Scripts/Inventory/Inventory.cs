718a8b3544f1af97da07179f983e4bf4f3b038c8
Obe107 <ozbej.zurman10@gmail.com>
2026-01-15 18:16:12 +0100

    Inventory Resource
    Added Inventory Resource that cointains an array of Inventory Items. Also added to the Main.cs to showcase usage

--- Scripts/Inventory/Inventory.cs
+++ Scripts/Inventory/Inventory.cs
@@ -1,6 +1,64 @@
 using Godot;
 using System;
 
+[GlobalClass]
 public partial class Inventory : Resource
 {
+    // vedno empty array kot default, da ne dobimo null reference exceptiona
+    [Export]
+    public InventoryItem[] Items { get; set; } = Array.Empty<InventoryItem>();
+
+
+    public void AddItem(InventoryItem item)
+    {
+        // Dodaj item v prvi prosti slot
+        for (int i = 0; i < Items.Length; i++)
+        {
+            // Ce je slot prazen, dodaj item
+            if (Items[i] == null)
+            {
+                Items[i] = item;
+                GD.Print($"Added item {item.Name} to slot {i}");
+                return;
+            }
+        }
+        GD.Print("No free slot available to add the item!");
+    }
+
+    public void UseItem(int index)
+    {
+        // Uporabi item na dolocenem indexu
+        if (index < 0 || index >= Items.Length)
+        {
+            GD.Print("Invalid index!");
+            return;
+        }
+
+        InventoryItem item = Items[index];
+        if (item != null)
+        {
+            item.UseItem();
+        }
+        else
+        {
+            GD.Print("No item in the selected slot to use!");
+        }
+    }
+
+    public void PrintAllItems()
+    {
+        // Izpisi vse iteme v inventoriju
+        for (int i = 0; i < Items.Length; i++)
+        {
+            if (Items[i] != null)
+            {
+                GD.Print($"Item {i}: {Items[i].Name}");
+            }
+
+            else
+            {
+                GD.Print($"Item {i}: none");
+            }
+        }
+    }
 }
--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -7,6 +7,10 @@ public partial class Main : Node2D
     [Export]
     public InventoryItem editor_item;
 
+    // Inventory narejen v editorju
+    [Export]
+    public Inventory editor_inv;
+
     public override void _Ready()
     {
         // Naredi iteme v scriptu
@@ -15,10 +19,32 @@ public partial class Main : Node2D
         
         InventoryItem potion = new ConsumableItem();
         potion.Name = "Health Potion";
-        
+
+        // Lahko uporabimo iteme direkt iz scripta ali pa v inventoriju (doli)
+
+        /*
         UseInventoryItem(simpleitem);
         UseInventoryItem(potion);
         UseInventoryItem(editor_item);
+        */
+
+        // Preden dodamo iteme v kodi (izpise samo iteme ki so bili dodani v editorju)
+        GD.Print(" --- Inventory before adding items in script:");
+        editor_inv.PrintAllItems();
+
+        // Dodamo iteme iz scripta
+        editor_inv.AddItem(simpleitem);
+        editor_inv.AddItem(potion); // Tu bo zmanjkalo prostora
+
+        // Ko dodamo iteme v scriptu
+        GD.Print(" --- Inventory after adding items in script:");
+        editor_inv.PrintAllItems();
+
+        // Uporabimo item iz inventorija
+        GD.Print(" --- Using item at index 0, 1 and 2:");
+        editor_inv.UseItem(0);
+        editor_inv.UseItem(1);
+        editor_inv.UseItem(2);
     }
 
     private void UseInventoryItem(InventoryItem item)
