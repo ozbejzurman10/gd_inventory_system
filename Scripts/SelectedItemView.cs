7a3c794140c5e3621db7d44c39439d474ca7f4a7
ozbejzurman10 <ozbej.zurman10@gmail.com>
2026-02-06 08:50:47 +0100

    UseSelectedItem Button
    Added UseSlectedItem Button to use the item currently selected.

--- Scripts/SelectedItemView.cs
+++ Scripts/SelectedItemView.cs
@@ -4,13 +4,24 @@ using System;
 public partial class SelectedItemView : Control
 {
     private InventorySlotGUI previewGuiSlot;
+    private Button useItemButton;
+    private InventoryItem selectedItem;
     public override void _Ready()
     {
         previewGuiSlot = GetNode<InventorySlotGUI>("Preview Slot");
+        useItemButton = GetNode<Button>("Use Item Button");
+
+        useItemButton.Pressed += UseSelectedItem;
     }
 
     public void UpdateDisplay(InventoryItem item, int amount)
     {
         previewGuiSlot.InsertItem(item, amount);
+        selectedItem = item;
+    }
+
+    public void UseSelectedItem()
+    {
+        selectedItem.Use();
     }
 }
