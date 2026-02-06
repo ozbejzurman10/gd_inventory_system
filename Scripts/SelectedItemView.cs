9b67fa2f75a7f9c7fbd70a7ee37a259ef640958f
ozbejzurman10 <ozbej.zurman10@gmail.com>
2026-02-06 10:36:32 +0100

    Selected Item Label

--- Scripts/SelectedItemView.cs
+++ Scripts/SelectedItemView.cs
@@ -6,10 +6,12 @@ public partial class SelectedItemView : Control
     private InventorySlotGUI previewGuiSlot;
     private Button useItemButton;
     private InventoryItem selectedItem;
+    private Label itemNameLabel;
     public override void _Ready()
     {
         previewGuiSlot = GetNode<InventorySlotGUI>("Preview Slot");
         useItemButton = GetNode<Button>("Use Item Button");
+        itemNameLabel = GetNode<Label>("Item Name Label");
 
         useItemButton.Pressed += UseSelectedItem;
     }
@@ -18,6 +20,13 @@ public partial class SelectedItemView : Control
     {
         previewGuiSlot.InsertItem(item, amount);
         selectedItem = item;
+
+        if (item != null)
+        {
+            itemNameLabel.Text = item.Name;
+        }
+
+        else itemNameLabel.Text = "No Item";
     }
 
     public void UseSelectedItem()
